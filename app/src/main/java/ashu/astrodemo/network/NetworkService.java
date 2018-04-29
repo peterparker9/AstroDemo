6b02d58ba92029a8c4dfe76eb06e431252eb701a
ashu_sharma <ashutoshsmartcool@gmail.com>
2018-04-30 04:23:08 +0530

    removing unnecessary libs

--- app/src/main/java/ashu/astrodemo/network/NetworkService.java
+++ app/src/main/java/ashu/astrodemo/network/NetworkService.java
@@ -1,7 +1,6 @@
 package ashu.astrodemo.network;
 
 import ashu.astrodemo.model.ResponseDTO;
-import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
